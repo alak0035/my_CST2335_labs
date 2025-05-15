2c690f61dfcf4c20656391b372d1db57e4dac6c8
Alexander Ala-Kantti <aalak0035@algonquinlive.com>
2025-05-15 12:08:02 -0400

    updated original code to match requirements for Lab 1

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var _counter = 0.0;
+  var myFontSize = 30.0;
+
+  void setNewValue(double value) {
+    setState(() {
+      _counter = value;
+      myFontSize = value;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -67,7 +75,9 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_counter < 99.0) {
+        _counter++;
+      }
     });
   }
 
@@ -108,11 +118,12 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            Text('You have pushed the button this many times:', style: TextStyle(fontSize: myFontSize)),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: TextStyle(fontSize: myFontSize),
             ),
+            Slider(value: _counter, onChanged: setNewValue, max:100.0, min: 0.0)
           ],
         ),
       ),
